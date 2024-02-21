a16054c4688c2b0596949af4a4330365e450f168
Jeffro Tanvik <00002@protonmail.com>
2024-02-21 19:53:00 +0800

    update

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import pymongo
+import os
+import time
+
+
+def main():
+    HOST = os.environ.get('MONGO_HOST', 'mongodb.railway.internal')
+    PORT = int(os.environ.get('MONGO_PORT', 27017))
+    UserName = os.environ.get('MONGO_USERNAME', None)
+    Password = os.environ.get('MONGO_PASSWORD', None)
+
+    client = pymongo.MongoClient(host=HOST, port=PORT, username=UserName, password=Password)
+    db = client.dexscreener
+
+    req = requests.get('https://dd.dexscreener.com/ds-data/chains/')
+    data = json.loads(req.text)
+    for chain in data:
+        name = chain['name']
+        doc = {
+            "name": name,
+            "data": chain,
+            "timestamp": time.time()
+        }
+        result = db.chains.find_one(
+            {"name": name}
+        )
+        if result is None:
+            db.chains.insert_one(
+                doc
+            )
+
+
+if __name__ == '__main__':
+    main()
